d3aa0ab11b159958cb317170dde9d1f252ee914a
sebastian <sebas99cano@gmail.com>
2021-06-15 15:14:20 -0500

    se agregaron validaciones en los value objects de videojeugo

--- src/main/java/co/com/sofka/tienda/videojuego/values/Adquirido.java
+++ src/main/java/co/com/sofka/tienda/videojuego/values/Adquirido.java
@@ -2,9 +2,31 @@ package co.com.sofka.tienda.videojuego.values;
 
 import co.com.sofka.domain.generic.ValueObject;
 
+import java.util.Objects;
+
 public class Adquirido implements ValueObject<Boolean> {
+
+    private final Boolean adquirido;
+
+    public Adquirido(Boolean adquirido) {
+        this.adquirido = adquirido;
+    }
+
     @Override
     public Boolean value() {
         return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Adquirido adquirido1 = (Adquirido) o;
+        return Objects.equals(adquirido, adquirido1.adquirido);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(adquirido);
     }
 }
